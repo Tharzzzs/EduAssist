36c6103dc06ce68ccc79ae63d226b93126ee27be
Kylene Arong <kylenearong131@gmail.com>
2025-12-08 19:21:19 +0800

    Improve Change Password UI and fix redirect behavior based on entry point

--- EduAssist/accounts/views.py
+++ EduAssist/accounts/views.py
@@ -103,19 +103,24 @@ def home_view(request):
 
 @login_required(login_url='login')
 def change_password(request):
+    next_page = request.GET.get("next", "profile")
+
     if request.method == 'POST':
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)
             messages.success(request, 'Your password was successfully updated!')
-            return redirect('dashboard')
+            if next_page == "settings":
+                return redirect('settings')
+            else:
+                return redirect('profile')
         else:
             messages.error(request, 'Please correct the errors below.')
     else:
         form = PasswordChangeForm(request.user)
 
-    return render(request, 'accounts/change_password.html', {'form': form})
+    return render(request, 'accounts/change_password.html', {'form': form, 'next': next_page,})
 
 
 
